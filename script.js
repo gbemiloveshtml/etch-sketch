d9ea26db300f49081dd048963ccba9de6d1c8257
Gbemi <preciousbolarinwa2026@gmail.com>
2024-03-04 22:15:49 -0600

    Allow user to build their own grid

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-//user input for page size
-let userInput = prompt(" Type in the number of squares per side ")
-
-
-//create div that includes all divs
+//create main div that contains all divs
 const mainDiv = document.createElement('div')
-mainDiv.setAttribute('id',"main")
+mainDiv.setAttribute('id','main')
 document.body.appendChild(mainDiv)
 
-
-//create layout of page
-const createPage = ()=>{
-    for(let i =0;i<16;i++){
-       const rowDiv = document.createElement('div');
-        rowDiv.setAttribute('id','row');
-        mainDiv.appendChild(rowDiv);
-
-        for(let j=0; j<16;j++){
-          const columnDiv = document.createElement('div');
-           columnDiv.setAttribute('id','column');
-           rowDiv.appendChild(columnDiv);
-           columnDiv.addEventListener('mouseover',function(){
-            columnDiv.style.backgroundColor='pink'
-           })
-            
-        }
-    }
+//create 16 x 16 divs using js ONLY
+const createPage = () => {
+//create button to ask user how many squares 
+    const userBtn = document.createElement('button')
+    userBtn.classList.add('btn')
+    document.body.appendChild(userBtn)
     
-};
-createPage()
 
-//create hover effect
+    //adds event listener to button and makes maximum square 100
+    userBtn.addEventListener('click', event =>{
+    let userSquare = prompt('How many squares do you want?')
+    if(userSquare>100){
+        alert("Squares cannot be more than 100, please try again")
+        userSquare = prompt('How many squares do you want?')
+    } 
+
+   
+    //creates number of sqaures that user asks for(basiclly the page)currently figuring out how to make amount of square asked for create a new set of grid
+    let numberOfSquares = userSquare
+    for(let i =0; i<numberOfSquares; i++){
+    const rowDiv = document.createElement('div')
+    console.log('added')
+    rowDiv.classList.add('row');
+    
+    for(let j =0;j<numberOfSquares;j++){
+        const columnDiv = document.createElement('div')
+        columnDiv.classList.add('column')
+        rowDiv.appendChild(columnDiv)
+        columnDiv.addEventListener('mouseover', event =>{
+            columnDiv.style.backgroundColor='pink'
+         
+          
+        })
+    }
+
+    mainDiv.appendChild(rowDiv)
+
+}
+}) 
+}
+
+
+createPage()
